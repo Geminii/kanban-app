ad9133e52473e1f965e5add4a9263904bda4bb5f
Jimmy Jouanne <jimmy.jouanne@gmail.com>
2020-11-10 09:08:51 +0100

    refactor: clean data object in options dropdown

--- components/dropdown/OptionsDropdown.vue
+++ components/dropdown/OptionsDropdown.vue
@@ -79,20 +79,6 @@ export default Vue.extend({
   data() {
     return {
       isDropdownOpen: false,
-      vcoConfig: {
-        handler: this.handler,
-        middleware: this.middleware,
-        events: ['dblclick', 'click'],
-        // Note: The default value is true, but in case you want to activate / deactivate
-        //       this directive dynamically use this attribute.
-        isActive: true,
-        // Note: The default value is true. See "Detecting Iframe Clicks" section
-        //       to understand why this behaviour is behind a flag.
-        detectIFrame: true,
-        // Note: The default value is false. Sets the capture option for EventTarget addEventListener method.
-        //       Could be useful if some event's handler calls stopPropagation method preventing event bubbling.
-        capture: false,
-      },
     }
   },
   computed: {
